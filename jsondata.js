95240e3a175945e195c4a0aa545123fff803771d
Reza Sadeghi <46601195+sadeghireza@users.noreply.github.com>
2021-08-05 23:12:48 +0430

    Create jsondata.js

---
+++ jsondata.js
@@ -0,0 +1,15 @@
+[{"Patient_info": {
+    "Patient_ID" : "raPatient0ID0",
+    "Last_Name"  : "Azari",
+    "First_Name" : "Reza",
+    "Medical_Condition": "Hypertension",
+    "Blood_Type"  : "B+",
+    "Age"  : "61 Years old"
+    "Weight"  : "130 Pounds"
+    "Height"  : "64 Inches"
+    "Telephone"  : "7034813211"
+    "Medical_CenterID": "WADC_GWUhosp"
+    "Health Insurer ID": "STATE_FARM"
+    }
+    }
+]
