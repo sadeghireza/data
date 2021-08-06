65e0949809dec87e15e84f29bd11fb21926cbcc9
Reza Sadeghi <46601195+sadeghireza@users.noreply.github.com>
2021-08-06 10:57:06 +0430

    Create jsondata_Allergy.js

---
+++ jsondata_Allergy.js
@@ -0,0 +1,11 @@
+ [{ "Name":"Acetaminophen",
+   "Type":"Medicine",
+   "Results_in":"Heart Palpitation"
+  },{
+    "Name":"Citrizine:",
+   "Type":"Medicine",
+   "Results_in":"Upset Digestive System"
+  }
+  ]
+    
+    
