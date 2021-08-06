05f50ec70d1c38b828cc8637dc18bdfbe88023de
Reza Sadeghi <46601195+sadeghireza@users.noreply.github.com>
2021-08-06 11:10:22 +0430

    Create jsondata_Emerg.js

---
+++ jsondata_Emerg.js
@@ -0,0 +1,10 @@
+[{
+  "First Name" : "RAecFN",
+  "Last Name" : "RAecLN",
+  "Telephone"  : "2023461292"
+},{
+  "First Name" : "PatFN",
+  "Last Name" : "RalfLN",
+  "Telephone"  : "62846528013"
+}
+ ]
