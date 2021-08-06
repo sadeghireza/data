62cc114014b98046bbf86d090f6ab963234def3e
Reza Sadeghi <46601195+sadeghireza@users.noreply.github.com>
2021-08-06 11:05:09 +0430

    Update jsondata_Vacc.js

--- jsondata_Vacc.js
+++ jsondata_Vacc.js
@@ -1,12 +1,12 @@
 [{
   "Name":"Test Moderna Corona Virus",
-  "Date":"Med Center1"
-  "Med_Center":"Dec. 21, 2020"
+  "Date":"Med Center1",
+  "Med_Center":"Dec. 21, 2020",
   "Description":},"COVID-19 First Inoculation"
  {
-  "Name":"Test Moderna Corona Virus 2nd Phase"
-  "Date":"Med Center2"
-  "Med_Center":"Jan. 10, 2020"
+  "Name":"Test Moderna Corona Virus 2nd Phase",
+  "Date":"Med Center2",
+  "Med_Center":"Jan. 10, 2020",
   "Description":"COVID-19 Second Inoculation"}
  ]
     
