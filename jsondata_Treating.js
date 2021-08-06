69daea74a9d0c3b92ac1c2bb7c32cb93c531aed0
Reza Sadeghi <46601195+sadeghireza@users.noreply.github.com>
2021-08-06 11:05:54 +0430

    Create jsondata_Treating.js

---
+++ jsondata_Treating.js
@@ -0,0 +1,7 @@
+[{
+  "First_Name": "raGWUcardTP1FN",
+   "Last Name" : "raGWUcardTP1LN",
+    "Specilty" : "Cardiologist",
+    "Telephone"  : "2023123103",
+    "email":   : "hosp_card_TP1@gwu.com"
+}]
